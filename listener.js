f25d6f0eadda45e905f579282c5e5ca6f7e4b0a9
Qjammer <baladoramiro+github@gmail.com>
2017-10-14 22:40:46 +0200

    check if transaction is in chain

--- listener.js
+++ listener.js
@@ -21,19 +21,21 @@ app.post("/checkCensus", (req, res) => {
 	} else {
 		res.send("Success");
 	}
-});//Need to generate new connections to spread the transaction
+});
 
-app.post("/checkBlocks", (req, res) => {
-	//Check blocks 
-	if(/*in blocks*/){
-		res.send("Error");
-	} else {
-		res.send("Success");
+app.post("/checkTransInBlocks", (req, res) => {
+	//Check if transaction is included in any previous blocks
+	let ret="Success";
+	blockChain.forEach( (block) => {
+		if(block.includedTransaction(req.body))	{
+			ret="Error";
+		}
 	}
+	res.send(ret);
 });
 
 app.post("/waitConsensus", (req, res) => {
-	//Check blocks 
+	//Wait for consensus in block history
 	if(/*not consensus*/){
 		res.send("Error");
 	} else {
@@ -51,7 +53,7 @@ app.post("/getTrans", (req, res) => {
 });
 
 app.post("/getBlock", (req, res) => {
-	//Receive transaction from peers
+	//Receive block from peers
 	if(/*wrong hash signature*/){
 		res.send("Error");
 	} else {
